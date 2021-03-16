f8b303926d8ed3c180c86e91e5bf94569288b5f9
mqois16 <qoisoctava@gmail.com>
2021-03-16 17:03:11 +0700

    add

--- app/src/main/java/com/example/android/tiltspot/MainActivity.java
+++ app/src/main/java/com/example/android/tiltspot/MainActivity.java
@@ -24,6 +24,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
@@ -41,6 +42,11 @@ public class MainActivity extends AppCompatActivity
     private TextView mTextSensorAzimuth;
     private TextView mTextSensorPitch;
     private TextView mTextSensorRoll;
+
+    private ImageView mSpotTop;
+    private ImageView mSpotBottom;
+    private ImageView mSpotLeft;
+    private ImageView mSpotRight;
 
     // Very small values for the accelerometer (on all three axes) should
     // be interpreted as 0. This value is the amount of acceptable
@@ -60,6 +66,10 @@ public class MainActivity extends AppCompatActivity
         mTextSensorAzimuth = (TextView) findViewById(R.id.value_azimuth);
         mTextSensorPitch = (TextView) findViewById(R.id.value_pitch);
         mTextSensorRoll = (TextView) findViewById(R.id.value_roll);
+        mSpotTop=findViewById(R.id.spot_top);
+        mSpotBottom=findViewById(R.id.spot_bottom);
+        mSpotLeft=findViewById(R.id.spot_left);
+        mSpotRight=findViewById(R.id.spot_right);
 
         // Get accelerometer and magnetometer sensors from the sensor manager.
         // The getDefaultSensor() method returns null if the sensor
@@ -126,9 +136,33 @@ public class MainActivity extends AppCompatActivity
         float pitch=orientationValues[1];
         float roll=orientationValues[2];
 
+        if (Math.abs(pitch)<VALUE_DRIFT){
+            pitch=0;
+        }
+        if (Math.abs(roll)<VALUE_DRIFT){
+            roll=0;
+        }
+
         mTextSensorAzimuth.setText(String.format("%1$.2",azimuth));
         mTextSensorPitch.setText(String.format("%1$.2",pitch));
         mTextSensorRoll.setText(String.format("%1$.2",roll));
+        mSpotTop.setAlpha(0f);
+        mSpotBottom.setAlpha(0f);
+        mSpotLeft.setAlpha(0f);
+        mSpotRight.setAlpha(0f);
+
+        if(pitch>0){
+            mSpotBottom.setAlpha(pitch);
+        }else {
+            mSpotTop.setAlpha(Math.abs(pitch));
+        }
+
+        if(roll>0){
+            mSpotLeft.setAlpha(roll;
+        }else {
+            mSpotRight.setAlpha(Math.abs(roll));
+        }
+
 
     }
 
